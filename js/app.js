833e70915e7e444fdc655ee6e004827e34b4c616
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-02 14:53:08 -0500

    finish html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  var addAppt = $('#add-appt-page').html();
+  var detailAppt = $('#appt-detail-page').html();
+
+  $('.main-view').html(addAppt);
+
+  $('.main-view')on('click', function)
+
+});
