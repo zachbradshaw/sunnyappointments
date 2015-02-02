aee5491d3f46add5bfa304af9597ddef2c934506
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-02 15:57:10 -0500

    implementing jquery functions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 $(function() {
 
+  var mainView = $('#main-page').html();
   var addAppt = $('#add-appt-page').html();
   var detailAppt = $('#appt-detail-page').html();
 
-  $('.main-view').html(addAppt);
+  $('.empty-wrapper').html(mainView);
 
-  $('.main-view')on('click', function)
+  // $('.add-appt').click(function() {
+  //   $('.main-view').html(addAppt);
+  // });
+
+  $('.empty-wrapper').on('click', '.add-appt',function() {
+    $('.main-view').html(addAppt);
+  });
+
+  // $('.add-appt-header').click(function() {
+  //   var button = $(this);
+  //   alert('fire');
+  //   $('.add-appt-view').html(mainView);
+  // });
+
+  $('.empty-wrapper').on('click', '.back-button', function(){
+
+    $('.main-view').html(mainView);
+  });
+
+  $('.empty-wrapper').on('click', '.appt-li', function() {
+    alert('heyo');
+    $('.main-view').html(detailAppt);
+  });
 
 });
