bdce8431c4b0278443535bd4726727a8879a4e5c
hifilorau <tomrau@gmail.com>
2015-02-08 16:55:59 -0500

    saving zach from himself

--- js/appointment.js
+++ js/appointment.js
@@ -1,22 +1,30 @@
-// The appointment constructor creates
+/// The appointment constructor creates
 // a new appointment.
 app.Appointment = function (spec) {
 
-  var self = {
-      title: spec.title,
-      date: spec.date,
-      time: spec.time,
-      streetAddress: spec.streetAddress,
-      city: spec.city,
-      state: spec.state,
-      timeId: (Date.now()),
-      fullAddress: spec.streetAddress + ' ' + spec.city + ', ' + spec.state,
+ var self = {
+     title: spec.title,
+     date: spec.date,
+     time: spec.time,
+     streetAddress: spec.streetAddress,
+     city: spec.city,
+     state: spec.state,
+     timeId: (Date.now()),
+     fullAddress: spec.streetAddress + ' ' + spec.city + ', ' + spec.state,
+     momentDate: moment(spec.date).format("YYYY-MM-DD"),
+     momentTime: moment(spec.time, ["h:mm A"]).format("HH:mm"),
 
-      equal: function (otherAppt) {
-        return self.timeId === otherAppt.timeId;
-      }
-  };
+     getApptTime: String(self.momentDate) + Strings(self.momentTime),
+     // momentDateTime: momentDate + momentTime,
+     // unixTime: moment.utc(momentDateTime),
 
-  return self;
+     equal: function (otherAppt) {
+       return self.timeId === otherAppt.timeId;
+     }
+ };
+
+ return self;
+
+
 
 };
--- js/show-list-page.js
+++ js/show-list-page.js
@@ -1,49 +1,51 @@
 app.showListPage = function () {
 
-  var mainView = $('#main-page').html();
-  var detailAppt = $('#appt-detail-page').html();
-  var appts = app.appointments.query();
-  var template = _.template(mainView, {variable: 'm'});
+ var mainView = $('#main-page').html();
+ var detailAppt = $('#appt-detail-page').html();
+ var appts = app.appointments.query();
+ var template = _.template(mainView, {variable: 'm'});
 
-  $('.empty-wrapper').html(template  ({
-     appts: appts
-  }));
+ // app.sort();
 
-  $('.add-appt').click(app.showAddPage);
+ $('.empty-wrapper').html(template  ({
+    appts: appts
+ }));
 
-  $('.remove-appt-button').click(function() {
-    var deleteThis = $('this > .confirm-delete-popup');
-    console.log(deleteThis);
-    // theLi = $(this).parent();
-    // var timeId = theLi.data('id');
-    // var appt = app.appointments.getById(timeId)
-    $(deleteThis).addClass('show-delete-popup');
-    // app.appointments.remove(appt);
-    // console.log(app.appointments.query());
-    // theLi.remove();
-    // app.showListPage();
-    // app.appointments.save();
-  });
+ $('.add-appt').click(app.showAddPage);
 
-  $('.appt-li').click(function() {
-    var item = $(this);
-    var timeId = item.data('id');
-    var appt = app.appointments.getById(timeId);
-    app.showDetailPage(appt);
-  });
+ $('.remove-appt-button').click(function() {
+   // var deleteThis = $(this > '.confirm-delete-popup');
+   // console.log(deleteThis);
+   theLi = $(this).parent();
+   var timeId = theLi.data('id');
+   var appt = app.appointments.getById(timeId)
+   // $(deleteThis).addClass('show-delete-popup');
+   app.appointments.remove(appt);
+   console.log(app.appointments.query());
+   theLi.remove();
+   app.showListPage();
+   app.appointments.save();
+ });
+
+ $('.appt-li').click(function() {
+   var item = $(this);
+   var timeId = item.data('id');
+   var appt = app.appointments.getById(timeId);
+   app.showDetailPage(appt);
+ });
 
 
-  // $('.remove-appt-button').click(function() {
-  //  theLi = $(this).parent();
-  //  var timeId = theLi.data('id');
-  //  var appt = app.appointments.getById(timeId)
-  //  app.appointments.remove(appt);
-  //  console.log(app.appointments.query());
-  //  theLi.remove();
-  //  app.showListPage();
-  //  app.appointments.save();
-  // });
+ // $('.remove-appt-button').click(function() {
+ //  theLi = $(this).parent();
+ //  var timeId = theLi.data('id');
+ //  var appt = app.appointments.getById(timeId)
+ //  app.appointments.remove(appt);
+ //  console.log(app.appointments.query());
+ //  theLi.remove();
+ //  app.showListPage();
+ //  app.appointments.save();
+ // });
 
-  app.timeMachine();
+ app.timeMachine();
 
 };
