c9c2b942f16fd58d17ca4321875a28654c52ad77
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-08 14:26:20 -0500

    adding confirm delete

--- js/show-list-page.js
+++ js/show-list-page.js
@@ -1,5 +1,4 @@
 app.showListPage = function () {
-  // app.timeMachine();
 
   var mainView = $('#main-page').html();
   var detailAppt = $('#appt-detail-page').html();
@@ -12,23 +11,38 @@ app.showListPage = function () {
 
   $('.add-appt').click(app.showAddPage);
 
-  $('.appt-li').click(function() {
-   var item = $(this);
-   var timeId = item.data('id');
-   var appt = app.appointments.getById(timeId);
-   app.showDetailPage(appt);
+  $('.remove-appt-button').click(function() {
+    var deleteThis = $('this > .confirm-delete-popup');
+    console.log(deleteThis);
+    // theLi = $(this).parent();
+    // var timeId = theLi.data('id');
+    // var appt = app.appointments.getById(timeId)
+    $(deleteThis).addClass('show-delete-popup');
+    // app.appointments.remove(appt);
+    // console.log(app.appointments.query());
+    // theLi.remove();
+    // app.showListPage();
+    // app.appointments.save();
   });
 
-  $('.remove-appt-button').click(function() {
-   theLi = $(this).parent();
-   var timeId = theLi.data('id');
-   var appt = app.appointments.getById(timeId)
-   app.appointments.remove(appt);
-   console.log(app.appointments.query());
-   theLi.remove();
-   app.showListPage();
-   app.appointments.save();
+  $('.appt-li').click(function() {
+    var item = $(this);
+    var timeId = item.data('id');
+    var appt = app.appointments.getById(timeId);
+    app.showDetailPage(appt);
   });
+
+
+  // $('.remove-appt-button').click(function() {
+  //  theLi = $(this).parent();
+  //  var timeId = theLi.data('id');
+  //  var appt = app.appointments.getById(timeId)
+  //  app.appointments.remove(appt);
+  //  console.log(app.appointments.query());
+  //  theLi.remove();
+  //  app.showListPage();
+  //  app.appointments.save();
+  // });
 
   app.timeMachine();
 
---
+++ js/sort.js
@@ -0,0 +1,8 @@
+app.sort = function () {
+
+  var item = $('.appt-li');
+  var date = new Date(item.data('date'));
+
+  if (date < currentDate) {
+
+};
